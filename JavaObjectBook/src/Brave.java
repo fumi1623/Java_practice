8842609cc8bef86bb41a6312a8fad76806635c27
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-07-09 15:24:01 +0900

    [finish]epilod 3

--- JavaObjectBook/src/Brave.java
+++ JavaObjectBook/src/Brave.java
@@ -3,22 +3,40 @@ public class Brave {
 	
 	String name;
 	int hp;
+	int level;
 //	MedicalHerb herb;
 //	
 //	void haveABreak() {
 //		this.hp += 10;
 //		System.out.println(this.name + "は休憩した。[HP=" + this.hp + "]");
 //	}
+
 //	コンストラクタ：newを使った時、自動で呼び出される
 //	メソットとクラス名が一致＆戻り値無し(voidも無し)
-	
 	Brave() {
 		this.name = "名前はまだ無い";
 		this.hp = 50;
+		this.level = 1;
+	}
+//	オーバーロード（上書き）：nameが入ってきたらこっちが実行される
+	Brave(String name) {
+//		this.hp = 50;
+//		this.level = 1;
+		this();
+		this.name = name;
 	}
 	
-	Brave(String name) {
-		this.name = name;
-		this.hp = 50;
+	Brave(String name, int hp) {
+//		this.name = name;
+//		this.level = 1;
+		this(name);
+		this.hp = hp;
+	}
+	
+	Brave(String name, int hp, int level) {
+//		this.name = name;
+//		this.hp = hp;
+		this(name, hp);
+		this.level = level;
 	}
 }
--- JavaObjectBook/src/World.java
+++ JavaObjectBook/src/World.java
@@ -9,10 +9,24 @@ public class World {
 //		alba.herb = herb;
 //		alba.herb.use(alba);
 		
-		Brave noName = new Brave();
-		Brave alba = new Brave("アルバ");
-		System.out.println(noName.name);
-		System.out.println(alba.name);
+//		Brave noName = new Brave();
+//		Brave alba = new Brave("アルバ");
+//		System.out.println(noName.name);
+//		System.out.println(alba.name);
 		
+//		Slime.price = 2;
+//		Slime slimeA = new Slime("スライムA");
+//		Slime slimeB = new Slime("スライムB");
+//		
+//		System.out.println(slimeA.price + ", " + slimeB.price);
+//		
+//		Slime.price = 4;
+//		System.out.println(slimeA.price + ", " + slimeB.price);
+//		
+//		Slime.price = 6;
+//		System.out.println(Slime.price);
+		
+		Slime.price = 2;
+		System.out.println(Slime.getPrize());
 	}
 }
